aeed9a462fd2bafa5785910086892c2880968c53
Miles Steele <miles@milessteele.com>
2017-04-17 22:58:03 -0400

    baby steps

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,21 @@
+extern crate docopt;
+extern crate rustc_serialize;
+
+use docopt::Docopt;
+
+const USAGE: &'static str = "
+Usage: bittles <torrent>
+";
+
+#[derive(RustcDecodable)]
+struct Args {
+    arg_torrent: String,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let args: Args = Docopt::new(USAGE)
+        .and_then(|d| d.decode())
+        .unwrap_or_else(|e| e.exit());
+
+    println!("torrent: {}", args.arg_torrent);
 }
